e495a7901c12a9ed258b09cce7820780e12c19e8
Benjamin E. Coe <bencoe@gmail.com>
2017-11-11 00:53:15 -0800

    feat: example of connecting to redis from our app (#4)

--- packages/badges-server/index.js
+++ packages/badges-server/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const client = require('redis').createClient(process.env.REDIS_URL)
+
+client.set('foo', '99', () => {})
 
 app.get('/', (req, res) => res.send("Hello I'm a badges server!"))
 
