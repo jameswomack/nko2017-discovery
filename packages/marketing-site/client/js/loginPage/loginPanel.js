8cbffdb3e5bdade6797b15065a63f7bddd0ece66
Benjamin E. Coe <bencoe@gmail.com>
2017-11-11 22:01:00 -0800

    chore: implemented OAuth 2.0 GithHub login flow and redis sessions (#12)

--- packages/marketing-site/client/js/loginPage/loginPanel.js
+++ packages/marketing-site/client/js/loginPage/loginPanel.js
@@ -4,7 +4,7 @@ import GHLogo from './ghLogo'
 const LoginPanel = () => {
   return (
     <div className="login-panel">
-      <a className="login-link" href="">
+      <a className="login-link" href="/auth">
         Login with Github
         <GHLogo width={ '30px' } height={ '30px' }/>
       </a>
--- packages/marketing-site/server.js
+++ packages/marketing-site/server.js
@@ -1,9 +1,35 @@
+require('dotenv').load()
+
 const path = require('path')
 const express = require('express')
 const packageJSON = require('./package')
-
+const client = require('redis').createClient(process.env.REDIS_URL)
 const app = express()
+
 const PORT = packageJSON.config.port
+
+// redis-backed sessions for fun and profit.
+// https://github.com/tj/connect-redis
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
+// store sessions in redis.
+app.use(session({
+  store: new RedisStore({
+    client: client
+  }),
+  secret: process.env.SESSION_SECRET || 'keyboard cat',
+  cookie: {
+    secure: process.NODE_ENV === 'production' ? true : false,
+    sameSite: 'lax'
+  },
+  resave: true,
+  saveUninitialized: true
+}))
+
+// parse application/json
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
 const log = (...msgs) => console.log(`[${packageJSON.name}]`, ...msgs)
 
@@ -30,6 +56,106 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/js/app.js', (req, res) => {
   res.sendFile(path.resolve(process.cwd(), 'js', 'app.js'))
+})
+
+// Github OAuth Login Routes.
+const GitHubApi = require('github')
+const github = new GitHubApi()
+const SimpleOauth = require('simple-oauth2')
+const oauth2 = SimpleOauth.create({
+  client: {
+    id: process.env.GITHUB_CLIENT_ID,
+    secret: process.env.GITHUB_CLIENT_SECRET
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize',
+  }
+})
+
+app.get('/auth', (req, res) => {
+  const authorizationUri = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: process.env.GITHUB_CALLBACK || 'http://127.0.0.1:8082/callback',
+    scope: 'read:user,user:email'
+  })
+  res.redirect(authorizationUri)
+})
+
+app.get('/callback', (req, res) => {
+  const code = req.query.code
+  const options = {code}
+
+  oauth2.authorizationCode.getToken(options, (error, result) => {
+    if (error) {
+      console.error('Access Token Error', error.message)
+      return res.status(400).json('Authentication failed')
+    }
+
+    const token = oauth2.accessToken.create(result)
+    github.authenticate({
+      type: 'oauth',
+      token: token.token.access_token
+    })
+
+    github.users.get({})
+      .then(rawUser => {
+        return saveUser(rawUser, token.token.access_token)
+      }).then((user) => {
+        req.session.user = user.name
+        return res.redirect('/')
+      }).catch(err => {
+        return res.status(500).json({
+          error: err.message
+        })
+      })
+  })
+})
+
+function saveUser (rawUser, authToken) {
+  const user = {
+    name: rawUser.data.login,
+    authToken: authToken,
+    avatar: rawUser.data.avatar_url,
+    email: rawUser.data.email,
+    bio: rawUser.data.bio
+  }
+  const update = []
+  Object.keys(user).forEach(key => {
+    update.push(key, user[key])
+  })
+  return new Promise((resolve, reject) => {
+    client.hset(user.name, update, (err, res) => {
+      if (err) return reject(err)
+      else return resolve(user)
+    })
+  })
+}
+
+// API endpoints.
+app.get('/v1/users/current', (req, res) => {
+  if (!req.session.user) {
+    return res.status(404).json({
+      error: 'not found'
+    })
+  } else {
+    client.hgetall(req.session.user, (err, user) => {
+      if (err) {
+        return res.status(500).json({
+          error: err.message
+        })
+      } else {
+        return res.status(200).json({
+          name: user.name,
+          avatar: user.avatar,
+          email: user.email,
+          bio: user.bio,
+          officeHoursBlurb: user.officeHoursBlurb || '',
+          badge: user.badge || ''
+        })
+      }
+    })
+  }
 })
 
 app.listen(PORT, () => {
